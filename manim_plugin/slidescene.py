ee0e55f49a6d682f097aec7ae24f74fea69a6797
RickDW <rickxdewolf@gmail.com>
2021-10-03 19:38:27 +0200

    <feat> WIP manim.js plugin; output JSON timestamps

--- manim_plugin/slidescene.py
+++ manim_plugin/slidescene.py
@@ -2,6 +2,7 @@ from manim import *
 
 import os
 import shutil
+import json
 
 
 config.video_dir= "./video_slides"
@@ -9,39 +10,47 @@ config.flush_cache = True
 config.disable_caching = True
 
 
-class SlideScene(Scene):
-    breaks=[0]
-    video_slides_dir="./video_slides"
+# TODO test this new implementation
+class PresentationScene(Scene):
     def setup(self):
-        super(SlideScene, self).setup()
-        self.breaks=[0]
+        super().setup()
+        self.breaks = [0]
+        self.video_slides_dir = config.video_dir
+        self.slide_name = type(self).__name__
 
-    def slide_break(self,t=0.5):
-        self.breaks+=[self.renderer.time+t/2]
+    def slide_break(self,t=1):
+        self.breaks += [self.renderer.time+t/2]
         self.wait(t)
 
-    def save_times(self):
-        self.breaks+=[self.renderer.time]
-        out=""
-        dirname=os.path.dirname(self.renderer.file_writer.movie_file_path)
+    def save_playback_info(self):
+        self.breaks += [self.renderer.time]
+        playback_info = {
+            "fragments": []
+        }
+        dirname = os.path.dirname(self.renderer.file_writer.movie_file_path)
+
         for i in range(len(self.breaks)-1):
-            out+=f"<p class=\"fragment\" type='video' time_start={self.breaks[i]} time_end={self.breaks[i+1]}></p>\n"
-        with open("%s/%s.txt"%(dirname,type(self).__name__),'w') as f:
-            f.write(out)
+            playback_info["fragments"].append({
+                "start": self.breaks[i],
+                "end": self.breaks[i+1],
+                "fragment-type": "normal"
+            })
+
+        with open("%s/%s.json" % (dirname, self.slide_name), 'w') as f:
+            f.write(json.dump(playback_info))
 
     def copy_files(self):
-        if self.video_slides_dir !=None:
+        if self.video_slides_dir != None:
             dirname=os.path.dirname(self.renderer.file_writer.movie_file_path)
-            slide_name = type(self).__name__
             if not os.path.exists(self.video_slides_dir):
                 os.makedirs(self.video_slides_dir)
-            shutil.copy2(os.path.join(dirname,"%s.mp4"%slide_name), self.video_slides_dir)
-            shutil.copy2(os.path.join(dirname,"%s.txt"%slide_name), self.video_slides_dir)
+            shutil.copy2(os.path.join(dirname,"%s.mp4" % self.slide_name), self.video_slides_dir)
+            shutil.copy2(os.path.join(dirname,"%s.json" % self.slide_name), self.video_slides_dir)
 
     def tear_down(self):
-        super(SlideScene, self).tear_down()
-        self.save_times()
+        super().tear_down()
+        self.save_playback_info()
 
     def print_end_message(self):
-        super(SlideScene, self).print_end_message()
+        super().print_end_message()
         self.copy_files()
--- reveal_plugin/manim.js
+++ reveal_plugin/manim.js
@@ -1,8 +1,47 @@
 window.RevealManim = {
   id: 'manim',
+  video_dir: 'videos',
   init: (deck) => {
     // TODO load the transition timestamps and add the fragments to the DOM
     // TODO set up the Reveal event listeners to handle fragment transitions
     // TODO test Manip and see whether the video generation process works
+
+    slides = Reveal.getSlides();
+    for (var i = 0; i<slides.length; i++) {
+      slide = slides[i];
+
+      if (slide.matches(".fv-background")) {
+        // this slide has a video playing as its background
+        video_path = slide.getAttribute("fv-video");
+        // TODO set up a local server so files can be loaded?
+        playback_info = load_playback_info(slide.getAttribute("fv-playback-info"));
+
+        // TODO implement
+      }
+      videos = slide.querySelectorAll(".fragment-video");
+
+      for (var j = 0; i < videos.length; j++) {
+        video = videos[j];
+        video_path = video.getAttribute("fv-video");
+        playback_info = load_playback_info(slide.getAttribute("fv-playback-info"));
+
+        // TODO set up the video
+      }
+    }
   }
+}
+
+function load_playback_info(url) {
+  var result = null;
+  var xmlhttp = new XMLHttpRequest();
+  // TODO use an asynchronous method instead of this
+  xmlhttp.open("GET", url, false);
+  xmlhttp.send();
+  if (xmlhttp.status==200) {
+    result = xmlhttp.responseText;
+  }
+  else {
+    console.error("Failed to load playback info from " + url);
+  }
+  return result;
 }

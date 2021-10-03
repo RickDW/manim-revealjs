2042862b6e71d3148daba99f3ba336d11945564c
RickDW <rickxdewolf@gmail.com>
2021-10-04 00:05:37 +0200

    <feat> WIP create a basic slide background setup

--- manim_plugin/manim_revealjs_presentations/test.py
+++ manim_plugin/manim_revealjs_presentations/test.py
@@ -10,5 +10,7 @@ class Testing(PresentationScene):
         """
         # TODO find out why end_fragment has the t parameter
         rect = mn.Rectangle(fill_color=mn.BLUE, fill_opacity=1)
-        self.add(rect)
+        self.play(mn.Create(rect))
         self.end_fragment(fragment_type=NORMAL)
+        self.play(rect.animate.shift(mn.UP).rotate(mn.PI / 3))
+        self.end_fragment()
--- reveal_plugin/manim.js
+++ reveal_plugin/manim.js
@@ -4,32 +4,126 @@ window.RevealManim = {
   init: (deck) => {
     // TODO load the transition timestamps and add the fragments to the DOM
     // TODO set up the Reveal event listeners to handle fragment transitions
-    // TODO test Manip and see whether the video generation process works
 
     slides = Reveal.getSlides();
     for (var i = 0; i<slides.length; i++) {
       slide = slides[i];
 
       if (slide.matches(".fv-background")) {
-        // this slide has a video playing as its background
+        // this slide should have a video playing as its background
         video_path = slide.getAttribute("fv-video");
-        // TODO set up a local server so files can be loaded?
         playback_info = load_playback_info(slide.getAttribute("fv-playback-info"));
+        fragments = playback_info["fragments"];
 
-        // TODO implement
+        // set up the video background
+        slide.setAttribute("data-background-video", video_path);
+        // TODO allow color customization?
+        // slide.setAttribute("data-background-color", "#000000");
+
+        for (var k = 0; k<fragments.length; k++) {
+          var data = fragments[k];
+          data["background-video"] = true;
+
+          var elem = create_HTML_fragment(data);
+          slide.appendChild(elem);
+        }
       }
-      videos = slide.querySelectorAll(".fragment-video");
 
-      for (var j = 0; i < videos.length; j++) {
+      // Set up individual video elements
+      videos = slide.querySelectorAll(".fv-video");
+
+      for (var j = 0; j < videos.length; j++) {
         video = videos[j];
-        video_path = video.getAttribute("fv-video");
-        playback_info = load_playback_info(slide.getAttribute("fv-playback-info"));
+        playback_info = load_playback_info(video.getAttribute("fv-playback-info"));
+        fragments = playback_info["fragments"];
 
-        // TODO set up the video
+        for (var k = 0; k<fragments.length; k++) {
+          var data = fragments[k];
+          data["background-video"] = false;
+
+          var elem = create_HTML_fragment(data);
+          video.appendChild(elem); //TODO find a better place to put these?
+        }
       }
     }
+
+
+    Reveal.addEventListener("fragmentshown", function(event) {
+      // TODO implement functionality for going through the slides backwards
+      if (event.fragment.matches(".fv-fragment")) {
+        var time_start = event.fragment.getAttribute("time_start");
+        var time_end = event.fragment.getAttribute("time_end");
+        var fragment_type = event.fragment.getAttribute("fragment_type");
+        var background_video = event.fragment.getAttribute("background_video") == "true";
+
+        if (background_video) {
+          var current_slide = Reveal.getCurrentSlide();
+          var video_elem = current_slide.slideBackgroundElement
+            .getElementsByTagName("video")[0];
+          
+          video_elem.ontimeupdate = function() {};
+          video_elem.currentTime = time_start;
+          video_elem.play();
+          video_elem.ontimeupdate = function() {
+            if (video_elem.currentTime - time_end > 0){
+              video_elem.pause();
+              video_elem.currentTime = time_end;
+              video_elem.ontimeupdate = function() {};
+            }
+          };
+        }
+        else {
+          // TODO implement functionality for standalone video elements
+        }
+      }
+    });
+
+    Reveal.addEventListener("fragmenthidden", function(event) {
+      // TODO figure out when this event occurs
+      if (event.fragment.matches(".fv-fragment")) {
+        var time_start = event.fragment.getAttribute("time_start");
+        var time_end = event.fragment.getAttribute("time_end");
+        var fragment_type = event.fragment.getAttribute("fragment_type");
+        var background_video = event.fragment.getAttribute("background_video") == "true";
+        
+        if (background_video) {
+          var current_slide = Reveal.getCurrentSlide();
+          var video_elem = current_slide.slideBackgroundElement
+            .getElementsByTagName("video")[0];
+          
+          video_elem.currentTime = time_start;
+          video_elem.pause();
+        }
+        else {
+          // TODO implement functionality for standalone video elements
+        }
+      }
+    });
+
+    Reveal.addEventListener("slidechanged", function(event) {
+      // TODO figure out when this event occurs
+      if (event.hasOwnProperty("fragment") && event.fragment.matches(".fv-fragment")) {
+        var time_start = event.fragment.getAttribute("time_start");
+        var time_end = event.fragment.getAttribute("time_end");
+        var fragment_type = event.fragment.getAttribute("fragment_type");
+        var background_video = event.fragment.getAttribute("background_video") == "true";
+        
+        if (background_video) {
+          var current_slide = Reveal.getCurrentSlide();
+          var video_elem = current_slide.slideBackgroundElement
+            .getElementsByTagName("video")[0];
+          
+          Reveal.navigateFragment(-1, 0);
+          Reveal.next();
+        }
+        else {
+          // TODO implement functionality for standalone video elements (??)
+        }
+      }
+    });
   }
 }
+
 
 function load_playback_info(url) {
   var result = null;
@@ -43,5 +137,23 @@ function load_playback_info(url) {
   else {
     console.error("Failed to load playback info from " + url);
   }
-  return result;
+  return JSON.parse(result);
+}
+
+
+function create_HTML_fragment(fragment_data) {
+  time_start = fragment_data["start"];
+  time_end = fragment_data["end"];
+  fragment_type = fragment_data["fragment-type"];
+  background_video = fragment_data["background-video"];
+
+  // create a fragment
+  var elem = document.createElement("div");
+  elem.classList = ["fragment fv-fragment"];
+  elem.setAttribute("time_start", time_start);
+  elem.setAttribute("time_end", time_end);
+  elem.setAttribute("fragment_type", fragment_type);
+  elem.setAttribute("background_video", background_video);
+
+  return elem;
 }

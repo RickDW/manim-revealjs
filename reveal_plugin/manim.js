14ebdb1a291c3c68f2f75d8016b4b2731203442c
RickDW <rickxdewolf@gmail.com>
2021-10-03 15:00:02 +0200

    <chore> add revealjs plugin todos

---
+++ reveal_plugin/manim.js
@@ -0,0 +1,8 @@
+window.RevealManim = {
+  id: 'manim',
+  init: (deck) => {
+    // TODO load the transition timestamps and add the fragments to the DOM
+    // TODO set up the Reveal event listeners to handle fragment transitions
+    // TODO test Manip and see whether the video generation process works
+  }
+}
